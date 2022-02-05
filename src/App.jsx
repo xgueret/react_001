a540e3760f437ca173f82649ff7cb2a188bfcab9
xgueret <971xavier.gueret@gmail.com>
2022-02-05 19:05:40 +0100

    components ProductList et Product

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import About from "./components/about/About";
 import Intro from "./components/intro/Intro";
+import ProductList from "./components/productList/ProductList";
 
 const App = () => {
   return <div>
     <Intro />
     <About />
+    <ProductList />
   </div>;
 };
 
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "./attention_copyright/product_sample_1.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "./attention_copyright/product_sample_2.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "./attention_copyright/product_sample_3.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "./attention_copyright/product_sample_4.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "./attention_copyright/product_sample_5.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "./attention_copyright/product_sample_6.png",
+      link: "http://lama.dev",
+    },
+  ];
